3ae250ee5cb2b1c7963f93ea62746b209dae504d
Tedozi Manson <duyphan.developer@gmail.com>
2017-02-06 01:46:02 +0700

    Upgrade to WebEd version 3.0

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
